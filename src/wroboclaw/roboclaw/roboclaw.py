a71d27202d8e6c9de3aa241025aace392caae2d4
Ben Nowotny <bennowotny65535@gmail.com>
2021-04-03 19:04:01 -0500

    Updated the Roboclaw initializer to accept a mock_warp parameter and pass that through to the RoboclawMock if applicable.

--- src/wroboclaw/roboclaw/roboclaw.py
+++ src/wroboclaw/roboclaw/roboclaw.py
@@ -2,5 +2,5 @@ from .mock import RoboclawChainMock
 from .model import RoboclawChain
 from .serial_interface import RoboclawChainSerial
 
-def init(com_port: str, baud: int, timeout: float = 0.01, mock: bool = False) -> RoboclawChain:
-    return RoboclawChainMock() if mock else RoboclawChainSerial(com_port, baud, timeout)
+def init(com_port: str, baud: int, timeout: float = 0.01, mock: bool = False, mock_warp: int = 1) -> RoboclawChain:
+    return RoboclawChainMock(mock_warp) if mock else RoboclawChainSerial(com_port, baud, timeout)
