9ef909e66d0c85198588bb1e739ca28631eed147
Ben Nowotny <bennowotny65535@gmail.com>
2021-04-03 19:01:08 -0500

    Updated the MockVelCtrl velocity controller to have a static TIME_WARP variable that affects how fast each encoder changes.

--- src/wroboclaw/util/mock.py
+++ src/wroboclaw/util/mock.py
@@ -7,6 +7,8 @@ from .util import BoundaryBehaviour, boundary_clamp
 class MockVelCtrl():
     """A mock implementation of a single velocity-controlled motor."""
 
+    TIME_WARP = 1
+
     def __init__(self, pos_bounds: Optional[Tuple[float, float]] = None, pos_clamp: BoundaryBehaviour = BoundaryBehaviour.clamp):    
         self.pos_bounds = pos_bounds
         self.pos_clamp = pos_clamp
@@ -16,7 +18,7 @@ class MockVelCtrl():
 
     def _update_pos(self):
         now = rospy.get_time()
-        self._current_pos += self._current_vel * (now - self._last_update_time)
+        self._current_pos += self._current_vel * (now - self._last_update_time) * MockVelCtrl.TIME_WARP
         if self.pos_bounds is not None:
             self._current_pos = boundary_clamp(self.pos_clamp, self._current_pos, *self.pos_bounds)
         self._last_update_time = now
