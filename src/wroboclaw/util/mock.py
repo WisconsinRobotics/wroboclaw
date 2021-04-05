8a61635d3e042554a5f83e5c2bebd0812926487b
Ben Nowotny <bennowotny65535@gmail.com>
2021-04-04 21:55:38 -0500

    Converted from the static `TIME_WARP` variable to an instance variable to track an independent max speed for each MockVelCtrl.

--- src/wroboclaw/util/mock.py
+++ src/wroboclaw/util/mock.py
@@ -7,21 +7,24 @@ from .util import BoundaryBehaviour, boundary_clamp
 class MockVelCtrl():
     """A mock implementation of a single velocity-controlled motor."""
 
-    TIME_WARP = 1
-
-    def __init__(self, pos_bounds: Optional[Tuple[float, float]] = None, pos_clamp: BoundaryBehaviour = BoundaryBehaviour.clamp):    
+    def __init__(self, pos_bounds: Optional[Tuple[float, float]] = None, pos_clamp: BoundaryBehaviour = BoundaryBehaviour.clamp, max_enc_speed: int = 1):    
         self.pos_bounds = pos_bounds
         self.pos_clamp = pos_clamp
         self._current_pos = 0.0
         self._current_vel = 0.0
+        self._max_enc_speed = max_enc_speed
         self._last_update_time = rospy.get_time()
 
     def _update_pos(self):
         now = rospy.get_time()
-        self._current_pos += self._current_vel * (now - self._last_update_time) * MockVelCtrl.TIME_WARP
+        self._current_pos += cast(int, self._current_vel / self.bounds[1]) * (now - self._last_update_time) * self._max_enc_speed
         if self.pos_bounds is not None:
             self._current_pos = boundary_clamp(self.pos_clamp, self._current_pos, *self.pos_bounds)
         self._last_update_time = now
+
+    def set_max_speed(self, max_speed: int):
+        self._update_pos()
+        self._max_enc_speed = max_speed
 
     def set_velocity(self, vel: float):
         self._update_pos()
