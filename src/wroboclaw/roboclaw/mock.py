1d98d2c2a9200f59d51f6ff27f7b91a2a76a0060
Ben Nowotny <bennowotny65535@gmail.com>
2021-04-06 20:54:14 -0500

    Moved speed factor logic to RoboclawMock over MockVelCtrl, removed phrases relating to 'encoder', removed RoboclawMock TODO.

--- src/wroboclaw/roboclaw/mock.py
+++ src/wroboclaw/roboclaw/mock.py
@@ -6,23 +6,26 @@ from ..util.util import BoundaryBehaviour
 from .model import Roboclaw, RoboclawChain, RoboclawChainApi        
 
 UINT32_BOUNDS = (0, (1 << 32) - 1)
+INT16_MAX = (1<<15)-1
 
 class RoboclawMock(Roboclaw):
     def __init__(self):
         self._left = MockVelCtrl(UINT32_BOUNDS, BoundaryBehaviour.wrap)
+        self._left_max_speed = 1
         self._right = MockVelCtrl(UINT32_BOUNDS, BoundaryBehaviour.wrap)
+        self._right_max_speed = 1
 
-    def set_enc_left_max_speed(self, max_enc_speed: int):
-        self._left.set_max_speed(max_enc_speed)
+    def set_enc_left_max_speed(self, max_speed: int):
+        self._left_max_speed = max_speed
 
     def set_enc_right_max_speed(self, max_enc_speed: int):
-        self._right.set_max_speed(max_enc_speed)
+        self._right_max_speed = max_speed
 
     def write_speed(self, spd_l: Optional[int] = None, spd_r: Optional[int] = None):
         if spd_l is not None:
-            self._left.set_velocity(spd_l) # TODO: probably needs some tuning factor here; possibly corrected in fix for issue #1
+            self._left.set_velocity(spd_l / INT16_MAX * self._left_max_speed)
         if spd_r is not None:
-            self._right.set_velocity(spd_r)
+            self._right.set_velocity(spd_r / INT16_MAX * self._left_max_speed)
 
     def read_encs(self) -> Tuple[Optional[int], Optional[int]]:
         return round(self._left.get_position()), round(self._right.get_position())
